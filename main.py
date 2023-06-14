bbaae0b457b15c93f12778e8938fc342e58754e0
DiegoPaulino <diegoiglan@hotmail.com>
2023-06-13 21:02:35 -0300

    Adcionando ibagens

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import pygame
+from pygame.locals import *
+from sys import exit
 
+
+pygame.init()
+
+largura = 500
+altura = 850
+
+tela = pygame.display.set_mode((altura, largura))
+pygame.display.set_caption('Enigma Quest')
+icone = pygame.image.load('img/icone.png')
+pygame.display.set_icon(icone)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.QUIT()
+            exit()
+    pygame.display.update()
